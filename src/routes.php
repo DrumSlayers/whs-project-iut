66e1b2839d533096b9704b8d5837c096baa0c3e4
Pierre 'DrumSlayer' Sarret <drumslayer@drumslayer.ovh>
2020-05-04 17:14:19 +0200

    Added "/API/assertion" route from Services chapter

--- src/routes.php
+++ src/routes.php
@@ -9,6 +9,36 @@ $app->get('/API', function(Request $request, Response $response) {
     echo "Welcome to whs API";
 });
 
+// API Assertion
+$app->get('/API/assertions', function(Request $request, Response $response, $args) {
+    // prepare the search query.
+    
+    $search=$request->getQueryParam('search');
+    if (strlen($search) == 0)
+      $search = '.';
+    // pdo
+    $settings = $this->get('settings')['db'];
+    $db = new PDO("mysql:host=" . $settings['host'] . ";dbname=" . $settings['dbname'] .";charset=utf8", $settings['user'], $settings['password']);
+    // SQL query
+    $search = str_replace(" ", "|", $search);
+    $query = $db->prepare("SELECT * FROM api_assertion WHERE sentence REGEXP :search");
+    $query->bindParam('search', $search);
+    $query->execute();
+    $result = $query->fetchAll();
+    $assertions = json_encode($result);
+
+    // Définition du content type de la page
+    $response = $response->withHeader('Content-type', 'application/json');
+    // copy to body de la response pour initialiser $body
+    $body = $response->getBody();
+    // écrire $body avec le json encodé
+    $body->write($assertions);
+    
+    return $response;
+  }
+);
+
+
 // Get an html view of assertions
 $app->get('/assertions', function (Request $request, Response $response, $args) {
     // prepare the search query
