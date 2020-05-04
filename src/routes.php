17f3979ae304ab57c9111dfc47df3a109dd685f2
Pierre 'DrumSlayer' Sarret <drumslayer@drumslayer.ovh>
2020-05-04 14:06:25 +0200

    Initial commit of project files

---
+++ src/routes.php
@@ -0,0 +1,33 @@
+<?php
+// Routes
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+// Get a simple message
+$app->get('/API', function(Request $request, Response $response) {
+    echo "Welcome to whs API";
+});
+
+// Get an html view of assertions
+$app->get('/assertions', function (Request $request, Response $response, $args) {
+    // prepare the search query
+    $search=$request->getQueryParam('search');
+    if (strlen($search) == 0)
+      $search = '.';
+    // pdo
+    $settings = $this->get('settings')['db'];
+    $db = new PDO("mysql:host=" . $settings['host'] . ";dbname=" . $settings['dbname'] .";charset=utf8", $settings['user'], $settings['password']);
+    // SQL query
+    $search = str_replace(" ", "|", $search);
+    $query = $db->prepare("SELECT * FROM api_assertion WHERE sentence REGEXP :search");
+    $query->bindParam('search', $search);
+    $query->execute();
+    $result = $query->fetchAll();
+    $assertions = json_encode($result);
+    //return $assertions;
+    // prepare data for the template
+    $args['assertions'] = json_decode($assertions);
+    return $this->renderer->render($response, 'assertions.phtml', $args);
+});
+
