7468d58820c36aa9cc640c5b29d8252d7b3b485c
Pierre 'DrumSlayer' Sarret <drumslayer@drumslayer.ovh>
2020-05-11 22:31:07 +0200

    Added POST, PUT and DELETE methodes to "/API/assertions"

--- src/routes.php
+++ src/routes.php
@@ -35,24 +35,120 @@ $app->get('/API/assertions', function(Request $request, Response $response, $arg
   }
 );
 
-// API Assertion with ID
+// API GET Assertion with ID (Get Assertion)
 $app->get('/API/assertions/[{id}]', function(Request $request, Response $response, $args) {
 
-    // SQL query with eloquent ORM
-    $result = App\Assertion::find($args['id']);
+  // SQL query with eloquent ORM
+  $result = App\Assertion::find($args['id']);
 
-    // encoding SQL query response to JSON
-    $assertions = json_encode($result);
+  // encoding SQL query response to JSON
+  $assertions = json_encode($result);
 
-    // define the response content header to json MIME type
-    $response = $response->withHeader('Content-type', 'application/json');
-    // initialise $body with a raw body from the response
-    $body = $response->getBody();
-    // write $body with the encoded json
-    $body->write($assertions);
+  // define the response content header to json MIME type
+  $response = $response->withHeader('Content-type', 'application/json');
+  // initialise $body with a raw body from the response
+  $body = $response->getBody();
+  // write $body with the encoded json
+  $body->write($assertions);
     
-    return $response;
+  return $response;
+}
+);
+
+// API POST Assertion with ID (Insert Assertion)
+$app->post('/API/assertions', function(Request $request, Response $response, $args) {
+
+  // insert body of the request into $body
+  $body = $request->getBody();
+
+  // decode request body's json into the $decoded_body array
+  $decoded_json = json_decode($body, true);
+
+  // prepare the ORM sql request
+  $assertion = new App\Assertion;
+
+  // Checking if data are set to avoid putting null data into the database
+  if(isset($decoded_json['topic'])){
+    $assertion -> topic = $decoded_json['topic']; // adding topic data to the topic row in DB 
   }
+  if(isset($decoded_json['sentence'])){
+    $assertion -> sentence = $decoded_json['sentence']; // adding sentence data to the sentence row in DB 
+  }
+  if(isset($assertion)){
+    $assertion -> save(); // saving the datas into the database
+  }
+  
+  $result_json = json_encode($assertion); // Feedback of the HTTP PUT request by re-encoding to JSON the $assertion object (checking errors)
+  
+
+  // define the response content header to json MIME type
+  $response = $response->withHeader('Content-type', 'application/json');
+  // initialise $body with a raw body from the response
+  $body = $response->getBody();
+  // write $body with the encoded json
+  $body->write($result_json);
+
+  return $response;
+}
+);
+
+// API PUT Assertion with ID (Modify Assertion)
+$app->put('/API/assertions/[{id}]', function(Request $request, Response $response, $args) {
+
+  // insert body of the request into $body
+  $body = $request->getBody();
+
+  // decode request body's json into the $decoded_body array
+  $decoded_json = json_decode($body, true);
+
+  // prepare the ORM sql request
+  $assertion = App\Assertion::find($args['id']);
+
+  // Checking if data are set to avoid putting null data into the database
+  if(isset($decoded_json['topic'])){
+    $assertion -> topic = $decoded_json['topic']; // adding topic data to the topic row in DB 
+  }
+  if(isset($decoded_json['sentence'])){
+    $assertion -> sentence = $decoded_json['sentence']; // adding sentence data to the sentence row in DB 
+  }
+  if(isset($assertion)){
+    $assertion -> save(); // saving the data into the database
+  }
+
+  $result_json = json_encode($assertion); // Feedback of the HTTP PUT request by re-encoding to JSON the $assertion object (checking errors)
+  
+  // define the response content header to json MIME type
+  $response = $response->withHeader('Content-type', 'application/json');
+  // initialise $body with a raw body from the response
+  $body = $response->getBody();
+  // write $body with the encoded json
+  $body->write($result_json);
+
+  return $response;
+}
+);
+
+// API DELETE Assertion with ID
+$app->delete('/API/assertions/[{id}]', function(Request $request, Response $response, $args) {
+
+    // Define the response content header to json MIME type
+    $response = $response->withHeader('Content-type', 'application/json');
+
+    // Send the existing line if exist to have a proof of what have been destroyed
+    $assertion = App\Assertion::find($args['id']);
+    if(isset($assertion)){
+      $result_json = json_encode($assertion); // Feedback of the HTTP PUT request by re-encoding to JSON the $assertion object (checking errors)
+      // initialise $body with a raw body from the response
+      $body = $response->getBody();
+      // write $body with the encoded json
+      $body->write($result_json);
+    }
+  
+  // Delete the entry with given ID in database
+  App\Assertion::destroy($args['id']);
+
+  return $response;
+}
 );
 
 // Get an html view of assertions
